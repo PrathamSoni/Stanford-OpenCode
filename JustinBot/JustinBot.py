8357b9e4d17d31ef71a90cddf33f996476729620
Justin Weiler <justincweiler@gmail.com>
2021-01-11 02:02:31 +0000

    Fixed up JustinBot to be accurate smh nikhil my head

--- JustinBot/JustinBot.py
+++ JustinBot/JustinBot.py
@@ -1,35 +1,73 @@
-#JustinBot.py
+# JustinBot.py
 """
-@author: Nikhil Devanathan
+@author: Nikhil Devanathan, Justin Weiler
 This script handles the functionality of JustinBot, a discord bot that dispenses imprompteau math lessons.
 """
-#Default packages
+# Default packages
 import os
 
-#Installed packages
+# Installed packages
 import discord
 from dotenv import load_dotenv
 
-#The bot secrets are stored in a local .env file for security
+# The bot secrets are stored in a local .env file for security
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
-#Fires when the bot connects to a serevr it has joined. Exists as a dev-side tool.
+# Fires when the bot connects to a serevr it has joined. Exists as a dev-side tool.
 @client.event
 async def on_ready():
     for guild in client.guilds:
         print(f'{client.user} has connected to {guild.name}!')
 
-#The bot always listens for messages, but it only responds when a message begins with ?soc
+# Function to tell if string is an int because apparently python doesn't have that natively.
+def is_int(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+# Function to give a multifactorial.
+def multifactorial(num, amount):
+    ret = 1
+    while num > 0:
+        ret *= num
+        num -= amount
+    return ret
+
+# Helper function for detecting a message where a lesson can be learned.
+def magic(word):
+    i = len(word) - 1
+    while i >= 0 and word[i] == '!':
+        i -= 1
+    i += 1
+    if i == 0:
+        return ''
+
+    if word.find('!') == i:
+        bef = word[:i]
+        ret = f'{word} = '
+        if is_int(bef):
+            val = multifactorial(int(bef), len(word) - i)
+            ret += str(val)
+        else:
+            ret += f'{bef}*({bef} - {len(word) - i}){word[i:]}'
+        return ret
+    else:
+        return ''
+
+# Fires whenever there's a message received.
 @client.event
 async def on_message(message):
     if '!' in message.content and message.author != client.user:
         words = message.content.split(' ')
         words[:] = [word for word in words if word != '' and word != '!']
         for word in words:
-            if word[-1] == '!':
-                await message.channel.send('%s = %s * (%s - 1)!' % (word, word[:-1], word[:-1]))
+            s = magic(word)
+            if s != '':
+                await message.channel.send(s)
 
-client.run(TOKEN) #Runs bot
+client.run(TOKEN) # Runs bot
